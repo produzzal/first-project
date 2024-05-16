b5ea09b7be75150d0b73b3f80f918debcb4820b3
prouzzal <prouzzal297@gmail.com>
2024-05-16 13:46:41 +0600

    student feature done

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,6 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 const app: Application = express();
 
 //parsers
@@ -7,8 +8,12 @@ const app: Application = express();
 app.use(express.json());
 app.use(cors());
 
+//applications routes
+app.use('/api/v1/students', StudentRoutes);
+
 app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World');
+  const a = 10;
+  res.send(a);
 });
 
 export default app;
